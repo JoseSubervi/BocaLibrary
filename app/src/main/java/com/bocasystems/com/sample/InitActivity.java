581568fcb341162b479c746673d3023e181dfb8f
jose.subervi <jose.subervi@primeweb.it>
2023-10-18 17:17:49 +0200

    KennedyV1.0

--- app/src/main/java/com/bocasystems/com/sample/InitActivity.java
+++ app/src/main/java/com/bocasystems/com/sample/InitActivity.java
@@ -3,23 +3,35 @@ package com.bocasystems.com.sample;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.lang.*;
-
-
+import it.custom.printer.api.android.*;
+import it.custom.printer.api.android.CustomPrinter;
 public class InitActivity extends Activity {
 
+    private CustomAndroidAPI prnDevice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_init);
-
+        
         ImageView IconGroup = findViewById(R.id.IconSettings);
         IconGroup.setColorFilter(getResources().getColor(R.color.colorSettings));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(InitActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
 
         IconGroup.setOnClickListener(new View.OnClickListener(){
             @Override
